99e608fa336c7972d1d99998e4ede46c54fda298
Jay Sorg <jay.sorg@gmail.com>
2025-08-31 23:28:14 -0700

    change to static build

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     // libcliprdr
-    const libcliprdr = b.addSharedLibrary(.{
+    const libcliprdr = b.addStaticLibrary(.{
         .name = "cliprdr",
         .root_source_file = b.path("src/libcliprdr.zig"),
         .target = target,
